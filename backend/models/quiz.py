14dae10dd351d68f9f38b7cca38ef95e1df4fc5d
Diego Barazarte <diegob.betancourt@gmail.com>
2026-01-12 12:08:31 -0500

    Los padres pueden ver los quizzes semanales, sin embargo, independientemente de la semana siempre se ve el mismo quiz

--- backend/models/quiz.py
+++ backend/models/quiz.py
@@ -2,7 +2,8 @@ from db import db
 
 class Quiz(db.Model):
     __tablename__ = "quizzes"
+
     id = db.Column(db.Integer, primary_key=True)
     clase_id = db.Column(db.Integer, nullable=False)
     titulo = db.Column(db.String(200), nullable=False)
-    fecha = db.Column(db.Date, nullable=False)
+    created_at = db.Column(db.DateTime, server_default=db.func.current_timestamp())
